ace1437f12f801f60e1df905ac36287826963435
Maximilian <scientificresearch309@gmail.com>
2023-06-16 23:14:39 +0200

    search to find secret data in js file from npm run build!

--- src/pages/PageMembers.tsx
+++ src/pages/PageMembers.tsx
@@ -2,6 +2,8 @@ export const PageMembers = () => {
 	return (
 		<div className="page pageMembers">
 			<p>This is the Members page.</p>
+			<p>Here is some secret information for members only!</p>
 		</div>
 	);
 };
+ 
