c2438defb7959733f20d23464b4ff78486495158
Maximilian <scientificresearch309@gmail.com>
2023-06-17 11:21:10 +0200

    and now, the differen users but not the unapproved one can see the member page

--- src/pages/PageMembers.tsx
+++ src/pages/PageMembers.tsx
@@ -1,23 +1,47 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useContext } from 'react';
+import { AppContext } from '../AppContext';
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const PageMembers = () => {
-	const [memberInfo, setMemberInfo] = useState({ message: '' });
+	const [pageInfo, setPageInfo] = useState({ message: '' });
+	const { currentUserIsInAccessGroup } = useContext(AppContext);
+
 	useEffect(() => {
-		(async () => {
-			setMemberInfo(
-				(await axios.get(`${backendUrl}/get-member-info`)).data
-			);
-		})();
+		if (currentUserIsInAccessGroup('unapprovedMembers')) {
+			(async () => {
+				setPageInfo(
+					(
+						await axios.get(
+							`${backendUrl}/get-unapproved-member-info`,
+							{
+								withCredentials: true,
+							}
+						)
+					).data
+				);
+			})();
+		}
+		if (currentUserIsInAccessGroup('members')) {
+			(async () => {
+				setPageInfo(
+					(
+						await axios.get(`${backendUrl}/get-member-info`, {
+							withCredentials: true,
+						})
+					).data
+				);
+			})();
+		}
 	}, []);
 
 	return (
 		<div className="page pageMembers">
 			{/* <p>This is the Members page.</p>
 			<p>Here is some secret information for members only!</p> */}
-			<p>{memberInfo.message}</p>
+			<p>{pageInfo.message}</p>
 		</div>
 	);
 };
